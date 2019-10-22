89e69e6e3d56ab469c774d6b9d3abd2623672024
Adrien Imberdis <AdrienImberdis@gmail.com>
2019-10-22 17:09:58 +0200

    Callback Game component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./components/reset.css";
+import Game from "./components/Game";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
 }
 
 export default App;
