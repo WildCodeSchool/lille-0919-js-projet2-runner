ad6a7b0b68bc80bc127ee93e241b63de6e5008bc
nassilio <hubmattfac@fac.com>
2019-10-25 12:05:00 +0200

    obstacles created, map modified for obstacles looping

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Map.scss";
+import Obstacles from "./Obstacles";
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 12,
+      y: 6,
+      transition: true
+    };
+    this.loopObstacle();
+  }
+
+  carte = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
+  loopObstacle() {
+    if (this.state.x === -2) {
+      this.setState({
+        x: this.state.x + 15,
+        transition: false
+      });
+    } else {
+      this.setState({
+        x: this.state.x - 1,
+        transition: true
+      });
+    }
+    setTimeout(this.loopObstacle.bind(this), 250);
+  }
+
+  render() {
+    return (
+      <div className="map">
+        {this.carte.map(row =>
+          row.map(column => {
+            return <div></div>;
+          })
+        )}
+        <Obstacles
+          x={this.state.x}
+          y={this.state.y}
+          transition={this.state.transition}
+        />
+      </div>
+    );
+  }
+}
+
+export default Map;
---
+++ frontend/src/components/Obstacles.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Obstacles.scss";
+
+class Obstacles extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div
+        className="bloc"
+        style={{
+          top: `${this.props.y * 64}px`,
+          left: `${this.props.x * 64}px`,
+          transitionDuration: `${this.props.transition ? "300ms" : "0ms"}`
+        }}
+      ></div>
+    );
+  }
+}
+
+export default Obstacles;
