419eea3acdc9d261695f96ec39a30fcd32d2b675
Adrien Imberdis <AdrienImberdis@gmail.com>
2019-10-29 10:39:05 +0100

    change class character by function character

--- frontend/src/components/Character.jsx
+++ frontend/src/components/Character.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import "./Character.scss";
 
-class Character extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div
-        className="character"
-        style={{
-          top: `${this.props.y * 64}px`,
-          left: `${this.props.x * 64}px`
-        }}
-      ></div>
-    );
-  }
+function Character(props) {
+  return (
+    <div
+      className="character"
+      style={{
+        top: `${props.y * 64}px`,
+        left: `${props.x * 64}px`
+      }}
+    ></div>
+  );
 }
 
 export default Character;
