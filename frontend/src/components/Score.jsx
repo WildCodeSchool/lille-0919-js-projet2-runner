8fadd0b76f132e973008de821f5ea3f95c5f1417
val-fcn <valentin.faucon@gmail.com>
2019-11-02 18:48:08 +0100

    change into an arrow function & add a pixeled font

--- frontend/src/components/Score.jsx
+++ frontend/src/components/Score.jsx
@@ -4,16 +4,21 @@ import "./Score.scss";
 class Score extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { score: 0 };
-    this.scoreUp();
+    this.state = {
+      score: 0
+    };
   }
 
-  scoreUp() {
-    this.setState({ score: this.state.score + 1 });
-    setTimeout(this.scoreUp.bind(this), 250);
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        score: this.state.score + 1
+      });
+    }, 1000);
   }
+
   render() {
-    return <p id="score">score : {this.state.score}</p>;
+    return <p id="score">score: {this.state.score}</p>;
   }
 }
 
