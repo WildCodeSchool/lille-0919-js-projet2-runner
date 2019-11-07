4f95abf73aa7504f594743b917c0e6ea88bf7e96
nassilio <hubmattfac@fac.com>
2019-11-07 15:24:22 +0100

    added modif to modals : size, close button, scrollbar

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,13 +11,61 @@ function Footer() {
             <li>Copyright © 2019</li>
             <Popup trigger={<li id="mentions">Mentions légales</li>} modal>
               {close => (
-                <div className="modal">
-                  <div className="content">
+                <div id="modal">
+                  <div id="closeModal" onClick={close}>
+                    x
+                  </div>
+                  <div id="content">
+                    <h2>Editeur du Site :</h2>
+                    <ul>
+                      <li>La Bobby Team </li>
+                      <li>Chez nous, la Bobby Factory</li>
+                      <li>Téléphone :06 35 41 88 02</li>
+                      <li>Email : s.dailliez@gmail.com /duhamelni@gmail.com</li>
+                      <li>Site Web : localhost</li>
+                    </ul>
+                    <div className="LinearGradientModal"></div>
+                    <h2>Hébergement :</h2>
+                    <ul>
+                      <li>Repository privé Github</li>
+                      <li>Site Web : localhost</li>
+                    </ul>
+                    <div className="LinearGradientModal"></div>
+                    <h2>Développement :</h2>
+                    <ul>
+                      <li>Wild Code School</li>
+                      <li>4 Avenue des Saules, Le Dôge, 59160 Lille</li>
+                      <li>Site Web : www.wildcodeschool.com</li>
+                    </ul>
+                    <div className="LinearGradientModal"></div>
+                    <h2>Conditions d’utilisation :</h2>
                     <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Corporis modi sunt vel explicabo cumque voluptatem sint
-                      magnam dolores incidunt, facilis totam pariatur quibusdam
-                      doloremque ut recusandae vitae maiores reprehenderit esse.
+                      Ce site est proposé en différents langages web (HTML,
+                      HTML5, Javascript, CSS, etc…) pour un meilleur confort
+                      d’utilisation et un graphisme plus agréable, nous vous
+                      recommandons de recourir à des navigateurs modernes comme
+                      Internet explorer (dernières versions), Firefox, Google
+                      Chrome, etc…
+                    </p>
+                    <h3>Cookies :</h3> L'accès au site ainsi qu'aux services
+                    associés ne nécessitent aucun recours aux cookies.
+                    <h3>Liens hypertextes :</h3>
+                    <p>
+                      Notre plateforme contient des liens vers d’autres sites
+                      internet ou d’autres ressources disponibles sur Internet.
+                      Bobby Factory ne dispose d’aucun moyen pour contrôler les
+                      sites en connexion avec ses sites internet. Elle ne peut
+                      être tenue pour responsable de tout dommage, de quelque
+                      nature que ce soit, résultant du contenu de ces sites ou
+                      sources externes, et notamment des informations, produits
+                      ou services qu’ils proposent, ou de tout usage qui peut
+                      être fait de ces éléments. Les risques liés à cette
+                      utilisation incombent pleinement à l’internaute, qui doit
+                      se conformer à leurs conditions d’utilisation. Les
+                      utilisateurs, les abonnés et les visiteurs des sites
+                      internet ne peuvent pas mettre en place un hyperlien en
+                      direction de ce site sans l’autorisation expresse et
+                      préalable de la Bobby Factory.
                     </p>
                   </div>
                 </div>
@@ -26,7 +74,7 @@ function Footer() {
             <a href="https://www.wildcodeschool.com/fr-FR">
               <li>By Wild Code School</li>
             </a>
-            <logo>
+            <span>
               <img
                 src="facebook_icon.png"
                 className="facebook-logo"
@@ -42,7 +90,7 @@ function Footer() {
                 className="twitter-logo"
                 alt="Twitter logo"
               />
-            </logo>
+            </span>
           </ul>
         </div>
       </div>
